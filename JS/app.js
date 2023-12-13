c7ed85cd37b8514d8ba8189c86c5aadf9cbb666a
kalle pettersson <kallle.v.pettersson@gmail.com>
2023-12-13 14:34:23 +0100

    navbarStarter 2023-12-13

---
+++ JS/app.js
@@ -0,0 +1,22 @@
+/***************************************************
+function that allows you to open and close the menu
+***************************************************/
+
+const togglebtn = document.querySelector('.toggle-btn');
+const togglebtnIcon = document.querySelector('.toggle-btn i');
+const droppdmenu = document.querySelector('.toggle-function-menu');
+let isOpen = false; 
+
+togglebtn.onclick = function () {
+    droppdmenu.classList.toggle('open');
+
+    isOpen = !isOpen; 
+
+    togglebtnIcon.className = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars';
+};
+
+/***********************************************
+the end to open and close the menu function code
+************************************************/
