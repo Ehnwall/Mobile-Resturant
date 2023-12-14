69e75a456ab773997ed54830d0e89ac07f2c3c3e
kalle pettersson <kallle.v.pettersson@gmail.com>
2023-12-14 12:40:19 +0100

    First commit cartSlider 2023-12-14 12:40

--- JS/app.js
+++ JS/app.js
@@ -5,18 +5,36 @@ function that allows you to open and close the menu
 const togglebtn = document.querySelector('.toggle-btn');
 const togglebtnIcon = document.querySelector('.toggle-btn i');
 const droppdmenu = document.querySelector('.toggle-function-menu');
-let isOpen = false; 
+let isOpen = false;
 
 togglebtn.onclick = function () {
     droppdmenu.classList.toggle('open');
 
-    isOpen = !isOpen; 
+    isOpen = !isOpen;
 
-    togglebtnIcon.className = isOpen
-        ? 'fa-solid fa-xmark'
-        : 'fa-solid fa-bars';
+    togglebtnIcon.className = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 };
 
 /***********************************************
 the end to open and close the menu function code
+************************************************/
+
+/***************************************************
+function that allows you to open and close cart
+***************************************************/
+
+let iconCart = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close');
+let body = document.querySelector('body');
+
+iconCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+});
+
+closeCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+});
+
+/***********************************************
+the end to open and close the cart
 ************************************************/
