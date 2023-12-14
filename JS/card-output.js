c17e892552de0fe53a62230d4c1280a5fd5ee97e
AwE9800 <143619434+AwE9800@users.noreply.github.com>
2023-12-14 11:04:38 +0100

    Added div element in javascript

--- JS/card-output.js
+++ JS/card-output.js
@@ -3,20 +3,62 @@ const paginte = (array, pageNumber, pageSize) => {
     return array.slice(startIndex, startIndex + pageSize);
 };
 
+const test = {
+    id: 'ribs-brisket-and-burnt-ends',
+    img: 'https://goldbelly.imgix.net/uploads/showcase_media_asset/image/79619/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1',
+    name: "Joe's KC BBQ",
+    dsc: "Joe's KC Ribs, Brisket & Burnt Ends",
+    price: 110.99,
+    rate: 4,
+    country: 'Kansas City, KS',
+};
+
 const createCard = (foodInfo) => {
     // create new div with class menu-card
-    /* <div class="menu-card">
-        <img
-            src="https://goldbelly.imgix.net/uploads/showcase_media_asset/image/79619/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1"
-            alt="Joe's KC BBQ" />
-        <h4>Joe's KC BBQ</h4>
-        <p>
-            <strong>Info:</strong> Joe's KC Ribs, Brisket & Burnt
-            Ends
-        </p>
-        <p><strong>Price:</strong> foodInfo.price $</p>
-        <p><strong>Country:</strong> foodInfo</p>
-        <p><strong>Rate:</strong> foodInfo</p>
-        <button class="btn-add-to-cart">foodInfo</button>
-    </div> */
+    const menuCardDiv = document.createElement('div');
+    menuCardDiv.classList.add('menu-card');
+
+    const menuCardImg = document.createElement(`img`);
+    menuCardImg.src = foodInfo.img;
+    menuCardImg.alt = foodInfo.name;
+
+    const menuCardName = document.createElement(`h4`);
+    menuCardName.textContent = foodInfo.name;
+
+    const menuCardInfo = document.createElement(`p`);
+    const strongInfo = document.createElement(`strong`);
+    strongInfo.textContent = `Info:`;
+    menuCardInfo.append(strongInfo, foodInfo.dsc);
+    // menuCardInfo.textContent = foodInfo.dsc;
+
+    const menuCardPrice = document.createElement(`p`);
+    const strongPrice = document.createElement(`strong`);
+    strongPrice.textContent = `Pris:`;
+    menuCardPrice.append(strongPrice, foodInfo.price, `kr`);
+
+    const menuCardCountry = document.createElement(`p`);
+    const strongCountry = document.createElement(`strong`);
+    strongCountry.textContent = `Country:`;
+    menuCardCountry.append(strongCountry, foodInfo.country);
+
+    const menuCardRate = document.createElement(`p`);
+    const strongRate = document.createElement(`strong`);
+    strongRate.textContent = `Rate:`;
+    menuCardRate.append(strongRate, foodInfo.rate);
+
+    const btnAddToCart = document.createElement(`button`);
+    btnAddToCart.classList.add(`btn-add-to-cart`);
+
+    menuCardDiv.append(
+        menuCardImg,
+        menuCardName,
+        menuCardInfo,
+        menuCardPrice,
+        menuCardCountry,
+        menuCardRate,
+        btnAddToCart
+    );
+    return menuCardDiv;
 };
+
+console.log(createCard(test));
