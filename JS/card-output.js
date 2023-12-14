5fab8c2024aafb619392a1474364899e53a66357
BomanStatic <boman.staticsites@gmail.com>
2023-12-14 11:27:36 +0100

    removed commented code

--- JS/card-output.js
+++ JS/card-output.js
@@ -9,16 +9,6 @@ const paginate = (array, pageNumber, pageSize) => {
     return array.slice(startIndex, startIndex + pageSize);
 };
 
-const test = {
-    id: 'ribs-brisket-and-burnt-ends',
-    img: 'https://goldbelly.imgix.net/uploads/showcase_media_asset/image/79619/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1',
-    name: "Joe's KC BBQ",
-    dsc: "Joe's KC Ribs, Brisket & Burnt Ends",
-    price: 110.99,
-    rate: 4,
-    country: 'Kansas City, KS',
-};
-
 const createCard = (foodInfo) => {
     // create new div with class menu-card
     const menuCardDiv = document.createElement('div');
@@ -33,26 +23,27 @@ const createCard = (foodInfo) => {
 
     const menuCardInfo = document.createElement(`p`);
     const strongInfo = document.createElement(`strong`);
-    strongInfo.textContent = `Info:`;
+    strongInfo.textContent = `Info: `;
     menuCardInfo.append(strongInfo, foodInfo.dsc);
     // menuCardInfo.textContent = foodInfo.dsc;
 
     const menuCardPrice = document.createElement(`p`);
     const strongPrice = document.createElement(`strong`);
-    strongPrice.textContent = `Pris:`;
+    strongPrice.textContent = `Pris: `;
     menuCardPrice.append(strongPrice, foodInfo.price, `kr`);
 
     const menuCardCountry = document.createElement(`p`);
     const strongCountry = document.createElement(`strong`);
-    strongCountry.textContent = `Country:`;
+    strongCountry.textContent = `Country: `;
     menuCardCountry.append(strongCountry, foodInfo.country);
 
     const menuCardRate = document.createElement(`p`);
     const strongRate = document.createElement(`strong`);
-    strongRate.textContent = `Rate:`;
+    strongRate.textContent = `Rate: `;
     menuCardRate.append(strongRate, foodInfo.rate);
 
     const btnAddToCart = document.createElement(`button`);
+    btnAddToCart.textContent = 'Beställ';
     btnAddToCart.classList.add(`btn-add-to-cart`);
 
     menuCardDiv.append(
@@ -74,12 +65,12 @@ const updatePage = () => {
         amountOfCards
     );
     const cardsContainer = document.querySelector('.menu-cards-container');
-    // cardsContainer.innerHTML = '';
+    cardsContainer.innerHTML = '';
     title.textContent = currentCategory;
 
     for (let food of pageData) {
-        // const card = createCard(food);
-        // cardsContainer.appendChild(card);
+        const card = createCard(food);
+        cardsContainer.appendChild(card);
     }
 };
 
@@ -90,14 +81,12 @@ console.log(db);
 const nextPageButton = document.querySelector('.next-page');
 nextPageButton.addEventListener('click', () => {
     currentPage++;
-    console.log(currentPage);
     updatePage();
 });
 
 const prevPageButton = document.querySelector('.prev-page');
 prevPageButton.addEventListener('click', () => {
     currentPage--;
-    console.log(currentPage);
     updatePage();
 });
 console.log(createCard(test));
